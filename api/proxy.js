e6e31c96de4f624cfde1799ab77b2c1defb986e0
naloz <gagayhhad@gmail.com>
2026-08-31 21:40:56 +0300

    Add debug flag

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const { url } = req.query;
+    const { url, debug } = req.query;
     
     if (!url) {
         return res.status(400).send('Missing url parameter');
@@ -8,21 +8,22 @@ export default async function handler(req, res) {
     const trackIdMatch = url.match(/\/tracks\/(\d+)/);
     const trackId = trackIdMatch ? trackIdMatch[1] : null;
 
+    let debugInfo = { url, trackId, listOk: false, hasTrack: false, error: null };
+
     if (trackId) {
         try {
             const baseUrl = `https://${req.headers.host}`;
             const listRes = await fetch(`${baseUrl}/list.json`);
             if (listRes.ok) {
+                debugInfo.listOk = true;
                 const list = await listRes.json();
                 
-                // list is an object: {"12345": "https://huggingface.co/...", ...}
                 if (list[trackId]) {
+                    debugInfo.hasTrack = true;
                     const hfUrl = list[trackId];
-                    // Example: https://huggingface.co/datasets/naloz/YMliberty/resolve/main/tracks/12345.mp3
-                    // We need the path after the host
                     let path = hfUrl;
                     try {
-                        path = new URL(hfUrl).pathname; // /datasets/naloz/YMliberty/resolve/main/tracks/12345.mp3
+                        path = new URL(hfUrl).pathname;
                     } catch(e) {}
                     
                     const xml = `<?xml version="1.0" encoding="utf-8"?>
@@ -33,13 +34,19 @@ export default async function handler(req, res) {
     <region>0</region>
     <s>0</s>
 </download-info>`;
-                    res.setHeader('Content-Type', 'application/xml');
-                    return res.status(200).send(xml);
+                    if (!debug) {
+                        res.setHeader('Content-Type', 'application/xml');
+                        return res.status(200).send(xml);
+                    }
                 }
             }
         } catch (e) {
-            console.error("Error fetching list:", e);
+            debugInfo.error = e.toString();
         }
+    }
+
+    if (debug) {
+        return res.status(200).json(debugInfo);
     }
 
     res.redirect(302, url);

ed9530f0d83f04dcbbc59f9043544d588c4d32ff
naloz <gagayhhad@gmail.com>
2026-09-07 11:57:45 +0300

    fix(api): use zero-latency pre-cached metadata for popular uncensored tracks

--- api/popular.js
+++ api/popular.js
@@ -1,21 +1,96 @@
-const axios = require("axios");
-
-// Curated list of iconic uncensored track IDs confirmed in YM Liberty DB
-const POPULAR_LIBERTY_IDS = [
-  "17198343", // Сектор Газа - Опять сегодня
-  "17198411", // Сектор Газа - Наркоман
-  "24442524", // Агата Кристи - Моряк
-  "25738208", // GUF - Выдох-вдох
-  "25168288", // ЛСП - Уровни
-  "21220262", // Anacondaz - Радуга
-  "21287099", // Anacondaz - Не учи меня как жить
-  "25319594", // Anacondaz - Мама, я люблю
-  "152973936", // ROCKET - Monday
-  "153116884", // ROCKET - Инкассатор
+// Curated list of iconic uncensored tracks confirmed in YM Liberty DB
+const STATIC_POPULAR_TRACKS = [
+  {
+    id: "152973936",
+    title: "Monday",
+    artists: "ROCKET",
+    durationMs: 168880,
+    coverUri: "https://avatars.yandex.net/get-music-content/20322863/61ba37c2.a.42778355-1/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "153116884",
+    title: "Инкассатор",
+    artists: "ROCKET",
+    durationMs: 165370,
+    coverUri: "https://avatars.yandex.net/get-music-content/17649213/197a9416.a.42837594-1/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "24442524",
+    title: "Моряк",
+    artists: "Агата Кристи",
+    durationMs: 215440,
+    coverUri: "https://avatars.yandex.net/get-music-content/17655650/39dc32dc.a.2849406-1/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "25738208",
+    title: "Выдох-вдох",
+    artists: "GUF",
+    durationMs: 170130,
+    coverUri: "https://avatars.yandex.net/get-music-content/42108/9c4d8789.a.3043667-1/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "25168288",
+    title: "Уровни",
+    artists: "ЛСП",
+    durationMs: 236210,
+    coverUri: "https://avatars.yandex.net/get-music-content/5282321/f1b045c0.a.17439799-1/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "21220262",
+    title: "Радуга",
+    artists: "Anacondaz, RasKar",
+    durationMs: 207370,
+    coverUri: "https://avatars.yandex.net/get-music-content/15682289/d2ad7f2a.a.479579-5/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "21287099",
+    title: "Не учи меня как жить",
+    artists: "Anacondaz, Тони Вечер",
+    durationMs: 173710,
+    coverUri: "https://avatars.yandex.net/get-music-content/16154377/7c85103a.a.5913573-4/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "25319594",
+    title: "Мама, я люблю",
+    artists: "Anacondaz",
+    durationMs: 224890,
+    coverUri: "https://avatars.yandex.net/get-music-content/15317937/82221d90.a.2982824-4/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "17198411",
+    title: "Наркоман",
+    artists: "Сектор Газа",
+    durationMs: 217960,
+    coverUri: "https://avatars.yandex.net/get-music-content/28589/e24b2353.a.1892834-1/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
+  {
+    id: "17198343",
+    title: "Опять сегодня",
+    artists: "Сектор Газа",
+    durationMs: 142220,
+    coverUri: "https://avatars.yandex.net/get-music-content/28589/e24b2353.a.1892834-1/400x400",
+    isLiberty: true,
+    explicit: true,
+  },
 ];
-
-let cachedTracks = null;
-let lastCacheTime = 0;
 
 module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -26,50 +101,5 @@ module.exports = async (req, res) => {
     return res.status(200).end();
   }
 
-  const now = Date.now();
-  if (cachedTracks && now - lastCacheTime < 3600000) {
-    return res.status(200).json({ tracks: cachedTracks });
-  }
-
-  try {
-    const tracksRes = await axios.post(
-      "https://api.music.yandex.net/tracks",
-      new URLSearchParams({ "track-ids": POPULAR_LIBERTY_IDS.join(",") }).toString(),
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "User-Agent": "YandexMusic/5.117.1 (Windows)",
-        },
-        timeout: 8000,
-      }
-    );
-
-    const rawTracks = tracksRes.data?.result || [];
-    const tracks = rawTracks.map((t) => {
-      const cover = t.coverUri
-        ? "https://" + t.coverUri.replace("%%", "400x400")
-        : t.albums?.[0]?.coverUri
-        ? "https://" + t.albums[0].coverUri.replace("%%", "400x400")
-        : "";
-
-      return {
-        id: t.id,
-        title: t.title,
-        version: t.version || "",
-        artists: (t.artists || []).map((a) => a.name).join(", "),
-        durationMs: t.durationMs || 0,
-        coverUri: cover,
-        isLiberty: true,
-        explicit: true,
-      };
-    });
-
-    cachedTracks = tracks;
-    lastCacheTime = now;
-
-    return res.status(200).json({ tracks });
-  } catch (err) {
-    console.error("Popular tracks fetch error:", err.message);
-    return res.status(500).json({ error: "Failed to fetch popular tracks" });
-  }
+  return res.status(200).json({ tracks: STATIC_POPULAR_TRACKS });
 };

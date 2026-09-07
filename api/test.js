69aa6bbcfc77633dedffefa475db0c5860b00798
naloz <gagayhhad@gmail.com>
2026-09-07 14:16:15 +0300

    test: use .ru domain

--- api/test.js
+++ api/test.js
@@ -1,9 +1,13 @@
 module.exports = async (req, res) => {
   const axios = require("axios");
   try {
-    const response = await axios.get("https://api.music.yandex.net/account/status", {
+    const response = await axios.get("https://api.music.yandex.ru/account/status", {
       timeout: 5000,
-      validateStatus: () => true // resolve all statuses
+      headers: {
+        "Authorization": "OAuth y0_AgAAAA_DUMMY",
+        "User-Agent": "com.yandex.music/5.117 (Android 13; samsung SM-G998B)"
+      },
+      validateStatus: () => true
     });
     res.json({
       success: true,
@@ -11,10 +15,6 @@ module.exports = async (req, res) => {
       data: response.data
     });
   } catch (err) {
-    res.json({
-      success: false,
-      error: err.message,
-      code: err.code
-    });
+    res.json({ success: false, error: err.message });
   }
 };
